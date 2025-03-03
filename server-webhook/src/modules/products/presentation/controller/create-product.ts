9c25e2f98b40577655b6bb39b5b4907f2379ef5e
Paulo Sergio <pauloserg861@gmail.com>
2025-03-03 15:38:32 -0300

    feat: ensure CreateProductController return 500 if CreateProductUseCase throws

--- server-webhook/src/modules/products/presentation/controller/create-product.ts
+++ server-webhook/src/modules/products/presentation/controller/create-product.ts
@@ -4,32 +4,37 @@ import {
   HttpResponse
 } from "../../../../shared/presentation/protocol";
 import { MissingParamError } from "../../../../shared/presentation/errors/missing-param-error";
-import { badRequest } from "../../../../shared/presentation/helpers/http-responses";
+import { badRequest, serverError } from "../../../../shared/presentation/helpers/http-responses";
 import { CreateProduct } from "../../domain/usecases/create-product";
 
 export class CreateProductController implements Controller {
   constructor(private readonly createProduct: CreateProduct) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, brand, description, price, quantity } = httpRequest.body;
-    const requiredFields = ['name', 'brand', 'description', 'price', 'quantity'];
-    for (const field of requiredFields) {
-      if (!httpRequest.body[field]) {
-        return badRequest(new MissingParamError(field));
+    try {
+      const { name, brand, description, price, quantity } = httpRequest.body;
+      const requiredFields = ['name', 'brand', 'description', 'price', 'quantity'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
       }
-    }
 
-    if (isNaN(price)) {
-      return badRequest(new Error('invalid param: price'));
-    }
+      if (isNaN(price)) {
+        return badRequest(new Error('invalid param: price'));
+      }
 
-    const productData = {
-      name,
-      brand,
-      description,
-      price,
-      quantity
-    };
-    await this.createProduct.execute(productData);
+      const productData = {
+        name,
+        brand,
+        description,
+        price,
+        quantity
+      };
+      await this.createProduct.execute(productData);
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
   }
 }
