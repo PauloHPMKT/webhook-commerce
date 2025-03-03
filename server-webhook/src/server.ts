d4735c6427df5473dceaa1030a4ac61b79837b51
Paulo Sergio <pauloserg861@gmail.com>
2025-03-03 20:15:33 -0300

    refactor: change import file

--- server-webhook/src/server.ts
+++ server-webhook/src/server.ts
@@ -1,4 +1,10 @@
-import { App } from "./modules/orders/main/app";
+import { App } from "./main/app";
+import { MongoHelper } from "./modules/products/infra/db/mongodb/helpers/mongo-client";
 
-const app = new App();
-app.initServer(3001);
+MongoHelper.connect('mongodb://localhost:27017/webhook-commerce')
+  .then(async () => {
+    console.warn('Connected to MongoDB');
+    const app = new App();
+    app.initServer(3001);
+  })
+  .catch(console.error);
