0e62d35d9842725661513ad8ce34422545e92998
Paulo Sergio <pauloserg861@gmail.com>
2025-03-03 14:33:15 -0300

    refactor: move global protocols to a simples export file

--- server-webhook/src/modules/products/presentation/controller/create-product.ts
+++ server-webhook/src/modules/products/presentation/controller/create-product.ts
@@ -1,6 +1,8 @@
-import { Controller } from "../../../../globals/protocol/controller";
-import { HttpRequest } from "../../../../globals/protocol/request";
-import { HttpResponse } from "../../../../globals/protocol/response";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from "../../../../globals/protocol";
 
 export class CreateProductController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
