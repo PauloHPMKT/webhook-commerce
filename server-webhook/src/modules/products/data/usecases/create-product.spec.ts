6311c83f0e278255efb460e53e79a609d6dc9716
Paulo Sergio <pauloserg861@gmail.com>
2025-03-03 17:54:55 -0300

    test: ensure CreateProduct throw an error if Product throws

--- server-webhook/src/modules/products/data/usecases/create-product.spec.ts
+++ server-webhook/src/modules/products/data/usecases/create-product.spec.ts
@@ -1,3 +1,4 @@
+import { CreateProductModel } from "../../domain/models/create-product";
 import { CreateProductUseCase } from "./create-product";
 
 const makeSut = () => {
@@ -12,4 +13,17 @@ describe('CreateProductUseCase', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
   });
+
+  it('should throw an erro if name is less than 3 characters', async () => {
+    const { sut } = makeSut();
+    const productData: CreateProductModel.Params = {
+      name: 'Pr',
+      brand: 'any_brand',
+      description: 'any_description',
+      price: 10,
+      quantity: 10,
+    };
+    const promise = sut.execute(productData);
+    await expect(promise).rejects.toThrow();
+  })
 });
