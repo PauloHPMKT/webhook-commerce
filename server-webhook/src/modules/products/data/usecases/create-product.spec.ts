76b96aff8f820b1e2b7b027bbb6354c832174f3b
Paulo Sergio <pauloserg861@gmail.com>
2025-03-03 18:19:39 -0300

    test: ensure CreateProductUseCase calls CreateProductRepository with correct values

--- server-webhook/src/modules/products/data/usecases/create-product.spec.ts
+++ server-webhook/src/modules/products/data/usecases/create-product.spec.ts
@@ -1,11 +1,42 @@
+import { Product } from "../../domain/entities/Product";
 import { CreateProductModel } from "../../domain/models/create-product";
+import { CreateProductRepository } from "../protocols/create-product-repository";
 import { CreateProductUseCase } from "./create-product";
 
-const makeSut = () => {
-  const sut = new CreateProductUseCase();
+const makeCreateProductRepository = (): CreateProductRepository => {
+  class CreateProductRepositoryStub implements CreateProductRepository {
+    async create(data: Product): Promise<CreateProductModel.Result> {
+      return {
+        id: data.id,
+        code: data.code,
+        name: data.name,
+        brand: data.brand,
+        description: data.description,
+        price: data.price,
+        quantity: data.quantity,
+        images: data.images,
+        discount: data.discount,
+        category: data.category,
+        status: data.status,
+        createdAt: data.createdAt,
+      };
+    }
+  }
+  return new CreateProductRepositoryStub();
+}
+
+const makeSut = (): SutTypes => {
+  const createProductRepository = makeCreateProductRepository();
+  const sut = new CreateProductUseCase(createProductRepository);
   return {
     sut,
+    createProductRepository,
   };
+}
+
+interface SutTypes {
+  sut: CreateProductUseCase;
+  createProductRepository: CreateProductRepository;
 }
 
 describe('CreateProductUseCase', () => {
@@ -40,5 +71,33 @@ describe('CreateProductUseCase', () => {
     const promise = sut.execute(productData);
     await expect(promise).rejects.toThrow();
     expect(promise).rejects.toThrow('price is not valid for a product');
-  })
+  });
+
+  it('should call productRepository with correct values', async () => {
+    const { sut, createProductRepository } = makeSut();
+    const createSpy = jest.spyOn(createProductRepository, 'create');
+    const productData: CreateProductModel.Params = {
+      name: 'valid_name',
+      brand: 'valid_brand',
+      description: 'valid_description',
+      price: 10,
+      quantity: 10,
+      images: null,
+      discount: null,
+      category: [],
+      status: Product.Status.ACTIVE,
+    };
+    await sut.execute(productData);
+    expect(createSpy).toHaveBeenCalledWith(expect.objectContaining({
+      name: 'valid_name',
+      brand: 'valid_brand',
+      description: 'valid_description',
+      price: 10,
+      quantity: 10,
+      images: null,
+      discount: null,
+      category: [],
+      status: Product.Status.ACTIVE,
+    }));
+  });
 });
