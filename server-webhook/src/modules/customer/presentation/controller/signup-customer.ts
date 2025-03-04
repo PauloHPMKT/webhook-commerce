1a0a2c77bdbe58d9bbb1048357fe3a3e13af88b8
Paulo Sergio <pauloserg861@gmail.com>
2025-03-04 14:47:04 -0300

    feat: ensure SignUpCustomerController is defined

---
+++ server-webhook/src/modules/customer/presentation/controller/signup-customer.ts
@@ -0,0 +1,7 @@
+import { Controller } from "../../../../shared/presentation/protocol";
+
+export class SignUpCustomerController implements Controller {
+  async handle(request: any): Promise<any> {
+    throw new Error("Method not implemented.");
+  }
+}
