a4aa903786cb140ca93b361b2a4f5c224ea06a7f
Paulo Sergio <pauloserg861@gmail.com>
2025-03-04 14:32:56 -0300

    feat: ensure AddToCartController calls AddToCartUseCase with correct values

---
+++ server-webhook/src/modules/cart/presentation/controller/add-to-cart.ts
@@ -0,0 +1,39 @@
+import {
+  ok,
+  badRequest,
+  serverError
+} from "../../../../shared/presentation/helpers/http-responses";
+import {
+  Controller,
+  HttpRequest,
+  HttpResponse
+} from "../../../../shared/presentation/protocol";
+import { AddToCartModel } from "../../domain/models/add-to-cart-model";
+import { AddToCart } from "../../domain/usecases/add-to-cart";
+import { MissingParamError } from "../../../../shared/presentation/errors/missing-param-error";
+
+export class AddToCartController implements Controller {
+  constructor(private readonly addToCart: AddToCart) {}
+
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+    try {
+      const { productId, quantity } = httpRequest.body;
+      const requiredFields = ['productId', 'quantity'];
+      for (const field of requiredFields) {
+        if (!httpRequest.body[field]) {
+          return badRequest(new MissingParamError(field));
+        }
+      }
+      const data: AddToCartModel.Params = {
+        productId,
+        quantity
+      };
+
+      const cart = await this.addToCart.execute(data);
+      return ok(cart);
+    } catch (error) {
+      console.error(error);
+      return serverError();
+    }
+  }
+}
