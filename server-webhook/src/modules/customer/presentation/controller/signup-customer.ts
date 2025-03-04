41ffe693439d26f67ee9a385d3651076e495571d
Paulo Sergio <pauloserg861@gmail.com>
2025-03-04 15:20:14 -0300

    feat: ensure SignUpCustomerController return 400 if a invalid email is provided

--- server-webhook/src/modules/customer/presentation/controller/signup-customer.ts
+++ server-webhook/src/modules/customer/presentation/controller/signup-customer.ts
@@ -5,8 +5,12 @@ import {
 } from "../../../../shared/presentation/protocol";
 import { MissingParamError } from "../../../../shared/presentation/errors/missing-param-error";
 import { badRequest } from "../../../../shared/presentation/helpers/http-responses";
+import { EmailValidator } from "../protocols/email-validator";
+import { InvalidParamError } from "../../../../shared/presentation/errors/invalid-param-error";
 
 export class SignUpCustomerController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const { name, email, password, passwordConfirmation } = httpRequest.body;
     const requiredFields = Object.keys({ name, email, password, passwordConfirmation });
@@ -14,6 +18,11 @@ export class SignUpCustomerController implements Controller {
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field));
       }
+    }
+
+    const isEmail = this.emailValidator.isValid(email);
+    if (!isEmail) {
+      return badRequest(new InvalidParamError('email'));
     }
   }
 }
