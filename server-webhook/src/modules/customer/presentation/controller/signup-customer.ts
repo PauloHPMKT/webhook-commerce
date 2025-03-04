2a79abf696570e3e74f1e4d41a36c4af9dfbb9f1
Paulo Sergio <pauloserg861@gmail.com>
2025-03-04 14:56:16 -0300

    feat: ensure SignUpCustomerController return 400 if no password is provided

--- server-webhook/src/modules/customer/presentation/controller/signup-customer.ts
+++ server-webhook/src/modules/customer/presentation/controller/signup-customer.ts
@@ -4,17 +4,15 @@ import {
   HttpResponse
 } from "../../../../shared/presentation/protocol";
 import { MissingParamError } from "../../../../shared/presentation/errors/missing-param-error";
+import { badRequest } from "../../../../shared/presentation/helpers/http-responses";
 
 export class SignUpCustomerController implements Controller {
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-    const { name, email } = httpRequest.body;
-    const requiredFields = ['name', 'email'];
+    const { name, email, password } = httpRequest.body;
+    const requiredFields = ['name', 'email', 'password'];
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
-        return {
-          statusCode: 400,
-          body: new MissingParamError(field)
-        }
+        return badRequest(new MissingParamError(field));
       }
     }
   }
