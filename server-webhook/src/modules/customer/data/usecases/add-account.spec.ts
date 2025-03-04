3571f92d93794599772ca991a776a6282325f181
Paulo Sergio <pauloserg861@gmail.com>
2025-03-04 19:33:35 -0300

    test: ensure AddAccountUseCase calls Encrypter with correct value

--- server-webhook/src/modules/customer/data/usecases/add-account.spec.ts
+++ server-webhook/src/modules/customer/data/usecases/add-account.spec.ts
@@ -1,19 +1,44 @@
+import { Encrypter } from "../protocols/encrypter";
 import { AddAccountUseCase } from "./add-account";
 
+const makeEncripter = (): Encrypter => {
+  class EncrypterStub implements Encrypter {
+    async hash(value: string): Promise<string> {
+      return 'hashed_password';
+    }
+  }
+  return new EncrypterStub();
+}
+
 const makeSut = (): SutTypes => {
-  const sut = new AddAccountUseCase();
+  const encrypter = makeEncripter();
+  const sut = new AddAccountUseCase(encrypter);
   return {
-    sut
+    sut,
+    encrypter,
   }
 }
 
 interface SutTypes {
   sut: AddAccountUseCase;
+  encrypter: Encrypter;
 }
 
 describe('AddAccountUseCase', () => {
   it('should be defined', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should call Encrypter with correct password', async () =>{
+    const { sut, encrypter } = makeSut();
+    const hashSpy = jest.spyOn(encrypter, 'hash');
+    const accountData = {
+      name: 'any_name',
+      email: 'any_email@mail.com',
+      password: 'any_password'
+    }
+    await sut.execute(accountData);
+    expect(hashSpy).toHaveBeenCalledWith('any_password');
   });
 });
