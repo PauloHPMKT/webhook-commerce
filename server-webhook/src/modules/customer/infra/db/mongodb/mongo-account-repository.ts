21761b7e14c65b84d8bc4fdc65b337f46f6c664d
Paulo Sergio <pauloserg861@gmail.com>
2025-03-05 21:11:08 -0300

    feat: ensure MongoAccountRepository creates a new customer account on success

--- server-webhook/src/modules/customer/infra/db/mongodb/mongo-account-repository.ts
+++ server-webhook/src/modules/customer/infra/db/mongodb/mongo-account-repository.ts
@@ -1,3 +1,5 @@
+import { ObjectId } from "mongodb";
+import { MongoHelper } from "../../../../../shared/infra/db/mongo-client";
 import { AddAccountRepository } from "../../../data/protocols/add-account-repository";
 import { FindCustomerRepository } from "../../../data/protocols/find-customer-repository";
 import { AddAccountRepositoryModel } from "../../../domain/models/add-account-repositpry";
@@ -7,7 +9,15 @@ export class MongoAccountRepository implements
   FindCustomerRepository
 {
   async add(data: AddAccountRepositoryModel.Params): Promise<AddAccountRepositoryModel.Result> {
-    throw new Error('Method not implemented.');
+    const customerCollection = MongoHelper.getCollection('customers');
+    const customerData = {
+      ...data,
+      _id: new ObjectId(data.id),
+    };
+    delete customerData.id;
+    await customerCollection.insertOne(customerData);
+    const customer = await customerCollection.findOne({ _id: customerData._id });
+    return MongoHelper.map(customer);
   }
 
   async findByEmail(email: string): Promise<boolean> {
