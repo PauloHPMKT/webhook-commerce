fe0d1da9f438b2dd0aad262c1f0ee7162a430cd1
Paulo Sergio <pauloserg861@gmail.com>
2025-03-07 21:50:13 -0300

    test: ensure AddToCartUseCase return 400 if no productId is provided

--- server-webhook/src/modules/cart/presentation/controller/add-to-cart.spec.ts
+++ server-webhook/src/modules/cart/presentation/controller/add-to-cart.spec.ts
@@ -1,3 +1,4 @@
+import { MissingParamError } from "../../../../shared/presentation/errors/missing-param-error";
 import { Controller } from "../../../../shared/presentation/protocol";
 import { AddToCartModel } from "../../domain/models/add-to-cart-model";
 import { AddToCart } from "../../domain/usecases/add-to-cart";
@@ -45,6 +46,18 @@ describe('AddToCartController', () => {
   it('should be defined', () => {
     const { sut } = makeSut();
     expect(sut).toBeDefined();
+  });
+
+  it('should return 400 if no productId is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        quantity: 1
+      }
+    }
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('productId'));
   });
 
   it('should call AddToCartUseCase with correct values', async () => {
